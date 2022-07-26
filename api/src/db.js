70cbeddd8305dcda52f6c6816b909e1b7b88e668
alexgz777 <alexgz777@gmail.com>
2022-07-26 14:28:23 -0300

    26/7/22

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-let sequelize =
+const sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
@@ -21,16 +21,17 @@ let sequelize =
         dialectOptions: {
           ssl: {
             require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
             rejectUnauthorized: false,
           },
           keepAlive: true,
         },
         ssl: true,
       })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-        { logging: false, native: false }
-      );
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+        logging: false,
+        native: false,
+      });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
